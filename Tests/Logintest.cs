1173d006e603d68f9a8809a57ffd05c3cae9a78f
Devi Learning <devi.learning@gmail.com>
2026-09-15 14:27:08 +0530

    commit changes

---
+++ Tests/Logintest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitPractice.Tests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void LoginTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
